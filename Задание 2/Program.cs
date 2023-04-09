60ca5ee21498a4e5a68909c15941d448056501af
Kirill_sheri <sheriffcities@icloud.com>
2023-04-09 18:27:45 +0300

    Сделал задачу 2

--- Задание 2/Program.cs
+++ Задание 2/Program.cs
@@ -4,12 +4,6 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
-
 Console.Clear();
 Console.Write("Введите А: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
